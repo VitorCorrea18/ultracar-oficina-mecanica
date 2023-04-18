db7f771aacd9fc1ce7b93d93abbe749875be66db
VitorCorrea18 <vitorcorrea18@gmail.com>
2023-04-18 16:33:29 -0300

    fix: get and pass client info to presenter

--- src/Components/ServiceCard/ServiceCardContainer.tsx
+++ src/Components/ServiceCard/ServiceCardContainer.tsx
@@ -1,24 +1,27 @@
 import { useEffect, useState } from 'react';
 import { IServiceCardContainerProps } from './interfaces';
-import { ICar } from '../../Interfaces';
-import { getCarByServiceId } from '../../Api'
+import { ICar, IClient } from '../../Interfaces';
+import { getCarByServiceId, getClientById } from '../../Api'
 import ServiceCardPresenter from './ServiceCardPresenter';
 
 export default function ServiceCardContainer({service}: IServiceCardContainerProps) {
   const [car, setCar] = useState<ICar>();
+  const [client, setClient] = useState<IClient>();
   
   useEffect(() => {
     if (service) {
-      const fetchCar = async () => {
+      const fetchClientCar = async () => {
         const car: ICar = await getCarByServiceId(service.carId);
         setCar(car);
+        const client: IClient = await getClientById(car.clientId);
+        setClient(client)
       };
-      fetchCar();
+      fetchClientCar();
     }
   /* eslint-disable react-hooks/exhaustive-deps */
   }, []);
 
-  if (!car) {
+  if (!car || !client) {
     return <div>Buscando as informações...</div>;
   }
 
@@ -26,6 +29,7 @@ export default function ServiceCardContainer({service}: IServiceCardContainerPro
     <ServiceCardPresenter
       service={service}
       car={car}
+      client={client}
     />
   );
 }
